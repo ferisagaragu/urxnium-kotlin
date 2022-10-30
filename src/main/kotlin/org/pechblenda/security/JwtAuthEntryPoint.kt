cc0da915ecb9a68959e4abb90346cc58b738e792
ferisagaragu <fernaisagaragu@gmail.com>
2022-10-30 13:20:46 -0600

    I fixed the error linked with the security about the new updates of spring boot

--- src/main/kotlin/org/pechblenda/security/JwtAuthEntryPoint.kt
+++ src/main/kotlin/org/pechblenda/security/JwtAuthEntryPoint.kt
@@ -6,17 +6,52 @@ import org.springframework.security.web.AuthenticationEntryPoint
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
+
 class JwtAuthEntryPoint: AuthenticationEntryPoint {
+
+	private val logger = LoggerFactory.getLogger(JwtAuthEntryPoint::class.java)
+
+	@Value("\${app.language:en}")
+	private lateinit var language: String
+
+	@Value("\${app.formal.language:}")
+	private lateinit var formalLanguage: String
+
+	@Value("\${message.auth.unauthorized-resource:}")
+	private lateinit var unauthorizedResource: String
 
 	override fun commence(
 		request: HttpServletRequest,
 		response: HttpServletResponse,
-		authenticationException: AuthenticationException
+		authException: AuthenticationException
 	) {
-		response.sendError(
-			HttpServletResponse.SC_UNAUTHORIZED,
-			authenticationException.message
-		)
+		logger.error("Unauthorized error: {}", authException.message)
+		println(language)
+		response.contentType = MediaType.APPLICATION_JSON_VALUE
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED)
+
+		val body: MutableMap<String, Any> = HashMap()
+		body["status"] = HttpServletResponse.SC_UNAUTHORIZED
+		body["error"] = "Unauthorized"
+		body["message"] = getUnauthorizedResource()
+		body["path"] = request.servletPath
+
+		val mapper = ObjectMapper()
+		mapper.writeValue(response.outputStream, body)
+	}
+
+	private fun getUnauthorizedResource(): String {
+		return when {
+			unauthorizedResource.isNotEmpty() -> unauthorizedResource
+			language == "es" ->
+				"${if (formalLanguage != "true") "Upss no" else "No"}  estas autorizado para acceder a este recurso"
+			else -> "${if (formalLanguage != "true") "Upss you" else "You"} are not authorized to access this resource"
+		}
 	}
 
 }
--- src/main/kotlin/org/pechblenda/security/JwtAuthTokenFilter.kt
+++ src/main/kotlin/org/pechblenda/security/JwtAuthTokenFilter.kt
@@ -1,18 +1,14 @@
 package org.pechblenda.security
 
-import org.slf4j.LoggerFactory
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.web.filter.OncePerRequestFilter
-
-import java.io.IOException
+import org.springframework.util.StringUtils
 
 import javax.servlet.FilterChain
-import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -21,42 +17,35 @@ class JwtAuthTokenFilter(
 	var userDetailsService: UserDetailsService
 ): OncePerRequestFilter() {
 
-	companion object {
-		private val logger = LoggerFactory.getLogger(JwtAuthTokenFilter::class.java)
-	}
-
-	@Throws(ServletException::class, IOException::class)
 	override fun doFilterInternal(
 		request: HttpServletRequest,
 		response: HttpServletResponse,
 		filterChain: FilterChain
 	) {
 		try {
-			val jwt = getJwt(request)
-			if (jwt != null && jwtProvider!!.validateJwtToken(jwt)) {
-				val username = jwtProvider.getUserNameFromJwtToken(jwt)
-				val userDetails: UserDetails = userDetailsService.loadUserByUsername(username)
+			val jwt: String? = parseJwt(request)
+			if (jwt != null && jwtProvider.validateJwtToken(jwt)) {
+				val username: String = jwtProvider.getUserNameFromJwtToken(jwt)
+				val userDetails: UserDetails? = userDetailsService?.loadUserByUsername(username)
 				val authentication = UsernamePasswordAuthenticationToken(
 					userDetails,
 					null,
-					userDetails.authorities
+					userDetails?.authorities
 				)
 
 				authentication.details = WebAuthenticationDetailsSource().buildDetails(request)
 				SecurityContextHolder.getContext().authentication = authentication
 			}
 		} catch (e: Exception) {
-			Companion.logger.error("Can NOT set user authentication")
+			logger.error("Cannot set user authentication: {}", e)
 		}
-
 		filterChain.doFilter(request, response)
 	}
 
-	private fun getJwt(request: HttpServletRequest): String? {
-		val authHeader = request.getHeader("Authorization")
-
-		return if (authHeader != null && authHeader.startsWith("Bearer ")) {
-			authHeader.replace("Bearer ", "")
+	private fun parseJwt(request: HttpServletRequest): String? {
+		val headerAuth = request.getHeader("Authorization")
+		return if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+			headerAuth.substring(7, headerAuth.length)
 		} else null
 	}
 
