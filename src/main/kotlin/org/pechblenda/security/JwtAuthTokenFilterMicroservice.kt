5f1dc81c046a1ef19b2d87cd7aa4aedf370d7914
ferisagaragu <fernaisagaragu@gmail.com>
2022-12-09 22:47:14 -0600

    I add on details jwt to use on other sub services

--- src/main/kotlin/org/pechblenda/security/JwtAuthTokenFilterMicroservice.kt
+++ src/main/kotlin/org/pechblenda/security/JwtAuthTokenFilterMicroservice.kt
@@ -3,13 +3,11 @@ package org.pechblenda.security
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
 
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
 import org.springframework.util.StringUtils
 import org.springframework.web.filter.OncePerRequestFilter
 
@@ -51,8 +49,8 @@ class JwtAuthTokenFilterMicroservice(
 						return out
 					}
 
-					override fun getDetails(): Any? {
-						return WebAuthenticationDetailsSource().buildDetails(request)
+					override fun getDetails(): String {
+						return jwt
 					}
 
 					override fun getPrincipal(): String {
